0de8932f5d8ff345a4419e6b0baeb88d13386b10
reza syarwani 2301010183 <rezasyarwani8@gmail.com>
2025-03-08 17:06:06 +0800

    project 1

---
+++ pertemuan1/src/pertemuan1/Pertemuan1.java
@@ -0,0 +1,21 @@
+ 
+package pertemuan1;
+
+/**
+ *
+ * @author Reza syarwani
+ * TGL:8 Maret 2025
+ */
+public class Pertemuan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("saya reza syarwani");
+       System.out.println("Nim saya 2301010183");
+       System.out.println("matakuliah yang saya ambil object oriented programing");
+       System.out.println("................. ");
+    }  
+    
+}
